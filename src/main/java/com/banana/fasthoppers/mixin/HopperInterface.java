6cfdae02c6f36e41e6d26a574f47169ddecada4f
Banana <66839366+BananaBaagel@users.noreply.github.com>
2023-09-05 13:49:42 -0400

    Remove Unnecessary import (#2)

--- src/main/java/com/banana/fasthoppers/mixin/HopperInterface.java
+++ src/main/java/com/banana/fasthoppers/mixin/HopperInterface.java
@@ -2,7 +2,6 @@ package com.banana.fasthoppers.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.gen.Invoker;
 
 import net.minecraft.block.entity.HopperBlockEntity;
 
